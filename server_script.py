2f5aac3e7bed22c233677bc91d119ff496dd867c
Munir Kakar <munir.kakar@hotmail.com>
2018-10-26 09:02:29 +0000

    Adding File

---
+++ server_script.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+import os
+dirame = 'git-folder'
+
+try:
+    os.mkdir(dirName)
+    print("Directory " , dirName ,  " Created ") 
+except FileExistsError:
+    print("Directory " , dirName ,  " already exists")
